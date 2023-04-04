e5e3b1a3a8fbfed43a1e58b1d50dfaaf252249d3
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-03 22:53:20 -0400

    tail.js use of array.slice()

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const tail = function(array) {
+  return array.slice(1);
+};
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3);
