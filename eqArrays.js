ee8bce8756c0a977ed0def41989b8facbd6f6c4f
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-05 09:01:18 -0400

    eqArray 2.0

--- eqArrays.js
+++ eqArrays.js
@@ -5,18 +5,22 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
       return false;
     }
-  }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+
+      return false;
+    }
+  } 
   return true;
-}
+};
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
