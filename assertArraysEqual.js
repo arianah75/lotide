ed7e3bd04ae819061ee9369121e9f486b35bf64c
Arian Ahmadi <arianahmadi1@gmail.com>
2023-04-15 14:22:56 -0400

    changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-      return false; //?
-    }
-    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-
 const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
@@ -22,8 +5,4 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
-
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = assertArraysEqual;
