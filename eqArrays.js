bfa152f8f1aa51644a70cd7b05e8cc56eef4027a
Arian <arianahmadi1@gmail.com>
2023-05-02 22:17:49 -0400

    changes added

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,17 @@
-const eqArrays = function (array1, array2) {
+const assertEqual = require("./assertEqual");
+
+const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
   }
 
   for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    } else if (array1[i] !== array2[i]) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
+
 
 module.exports = eqArrays;
