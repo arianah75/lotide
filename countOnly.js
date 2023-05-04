22d0c3bf8516493dd5ad37abc2b7cf9a34e975c4
Arian <arianahmadi1@gmail.com>
2023-05-03 22:17:40 -0400

    update and refactor

--- countOnly.js
+++ countOnly.js
@@ -1,25 +1,43 @@
-const countOnly = function (allItems, itemsToCount) {
-  const results = {};
+const assertEqual = require("./assertEqual");
 
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
   for (const item of allItems) {
-    console.log(item); // alt way for it  is results[item]++
+    console.log(item);
     if (itemsToCount[item]) {
       console.log(
         `the items that are also found in itemsToCount object is: ${item}`
       );
 
-      // inside the loop,
-      // increment the counter for each item:
-      //   set a property with that string key to:
-      //     the value that was already there (or zero if nothing there) with 1 added to it.
-
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
+      results[item] ? results[item] += 1 : results[item] = 1;
     }
   }
   return results;
 };
+
+////////// Test ////////////
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
 module.exports = countOnly;
