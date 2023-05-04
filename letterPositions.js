bc8b538bdfdeef837410b544f4ef632fc8d256c9
Arian <arianahmadi1@gmail.com>
2023-05-04 00:19:28 -0400

    refactoring

--- letterPositions.js
+++ letterPositions.js
@@ -1,16 +1,22 @@
-const letterPositions = function (sentence) {
-  const results = {};
-  for (let index in sentence) {
-    const character = sentence[index];
+const assertArraysEqual = require("./assertArraysEqual");
 
-    if (!(character === " ")) {
-      if (!results[character]) {
-        results[character] = [];
-      }
-      results[character].push(Number(index));
+const letterPositions = (sentence) => {
+  const results = {};
+  for (const index in sentence) {
+    const letter = sentence[index];
+
+    if (letter !== " ") {
+      results[letter] === undefined
+        ? (results[letter] = [Number(index)])
+        : results[letter].push(Number(index));
     }
   }
   return results;
 };
+
+///////// Test ////////////
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+
 
 module.exports = letterPositions;
