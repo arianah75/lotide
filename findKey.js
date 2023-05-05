e5d219c37e90009c95326e27459b728578d9fb69
Arian <arianahmadi1@gmail.com>
2023-05-05 00:22:32 -0400

    update

--- findKey.js
+++ findKey.js
@@ -1,10 +1,28 @@
-const findKey = function (object, callback) {
-  let keysArr = Object.keys(object);
-  for (let i = 0; i < keysArr.length; i++) {
-    if (callback(object[keysArr[i]])) {
-      return keysArr[i];
+const assertEqual = require("./assertEqual");
+
+const findKey = (object, callback) => {
+  let result = undefined;
+  for (let key in object) {
+    if (callback(object[key])) {
+      return (result = key);
     }
   }
-  return undefined;
+  return result;
 };
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      "noma": { stars: 2 },
+      "elBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
+
 module.exports = findKey;
