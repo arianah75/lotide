12249486c059d2e3d648cd017036b56e8f48b9e4
Arian <arianahmadi1@gmail.com>
2023-05-04 23:43:05 -0400

    updates

--- map.js
+++ map.js
@@ -1,8 +1,17 @@
-const map = function (array, callback) {
+const assertArraysEqual = require('./assertArraysEqual');
+
+const map =  (array, callback) => {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
 };
+
+///////////////TEST///////////////////
+
+// const words = ["ground", "control", "to", "major", "tom"];
+// const results1 = map(words, word => word[0]);
+// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
 module.exports = map;
