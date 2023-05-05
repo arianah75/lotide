4d7a201a3e8521d52ef7418bc32c18b85b4473ec
Arian <arianahmadi1@gmail.com>
2023-05-05 00:05:04 -0400

    takeUntil function added

--- takeUntil.js
+++ takeUntil.js
@@ -12,7 +12,7 @@ const takeUntil = (array, callback) => {
 
 module.exports = takeUntil;
 
-//TEST
+//////////TEST///////////
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, (x) => x < 0);
 
