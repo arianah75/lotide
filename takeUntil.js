5e46bf04b440d487aceb881bbe2e1cdc1295c9d8
Arian <arianahmadi1@gmail.com>
2023-05-05 00:03:59 -0400

    takeUntil function added

--- takeUntil.js
+++ takeUntil.js
@@ -1,12 +1,34 @@
-const takeUntil = function (array, callback) {
+const assertArraysEqual = require("./assertArraysEqual");
+
+const takeUntil = (array, callback) => {
   let result = [];
-  for (let item of array) {
-    if (!callback(item)) {
-      result.push(item);
-    } else {
-      break;
+  for (const item of array) {
+    if (callback(item)) {
+      return result;
     }
+    result.push(item);
   }
-  return result;
 };
+
 module.exports = takeUntil;
+
+//TEST
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
